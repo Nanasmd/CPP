c2cc62f484f396f02c90da7e2988f71b05bb6eb2
Nanasmd <81428378+Nanasmd@users.noreply.github.com>
2023-11-30 00:57:11 +0100

    Add files via upload

--- cpp_module_09/ex01/RPN.cpp
+++ cpp_module_09/ex01/RPN.cpp
@@ -6,81 +6,152 @@
 /*   By: nasamadi <nasamadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 15:41:17 by nasamadi          #+#    #+#             */
-/*   Updated: 2023/11/24 17:17:20 by nasamadi         ###   ########.fr       */
+/*   Updated: 2023/11/30 00:55:07 by nasamadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
 // Vérifie si la chaîne représente un nombre valide.
+// bool isValidNumber(std::string const &in)
+// {
+// 	if (in.empty() || (in.find_first_not_of(VALID_SET) != std::string::npos))
+// 		return (false);
+// 	return (true);	
+// }
+
+// // Vérifie si la chaîne est un opérateur valide.
+// bool isOperator(std::string const &in)
+// {
+// 	return (in == "+" || in == "-" || in == "*" || in == "/");
+// }
+
+// // Effectue l'opération indiquée sur les deux nombres en haut de la pile.
+// void doOperation(std::string const &in, std::stack<int> &stack)
+// {
+// 	if (stack.size() < 2)
+// 		throw std::runtime_error(INVALID);
+// 	int a = stack.top();
+// 	stack.pop();
+// 	int b = stack.top();
+// 	stack.pop();
+
+// 	if (in == "+")
+// 		stack.push(b + a);
+// 	else if (in == "-")
+// 		stack.push(b - a);
+// 	else if (in == "*")
+// 		stack.push(b * a);
+// 	else if (in == "/") {
+//         // Check for division by zero
+//         if (a == 0)
+//             throw std::runtime_error("Division by zero error");
+//         stack.push(b / a);
+//     }
+// }
+
+// // Ajoute un nombre à la pile.
+// void addNumber(std::string const &in, std::stack<int> &stack)
+// {
+// 	int nb = std::atoi(in.c_str());
+
+// 	if (nb > 9)
+// 		throw std::runtime_error(ERROR);
+// 	stack.push(nb);
+// }
+
+// // Traite l'expression RPN et imprime le résultat.
+// void rpn(std::string const &in)
+// {
+// 	std::stack<int> stack;
+// 	std::stringstream ss(in);
+// 	std::string token;
+
+// 	while (!ss.eof())
+// 	{
+// 		std::getline(ss, token, ' ');
+// 		if (token.empty())
+// 			continue;
+// 		if (isOperator(token))
+// 			doOperation(token, stack);
+// 		else if (isValidNumber(token))
+// 			addNumber(token, stack);
+// 		else
+// 			throw std::runtime_error(ERROR);
+// 	}
+// 	if (stack.size() == 1)
+// 		std::cout << stack.top() << std::endl;
+// 	else
+// 		throw std::runtime_error(ERROR);
+// }
+
 bool isValidNumber(std::string const &in)
 {
-	if (in.empty() || (in.find_first_not_of(VALID_SET) != std::string::npos))
-		return (false);
-	return (true);	
+    if (in.empty())
+        throw std::runtime_error("Error: Empty input");
+    if (in.find_first_not_of(VALID_SET) != std::string::npos)
+        throw std::runtime_error(NON_NUMERIC);
+    return true;    
 }
 
-// Vérifie si la chaîne est un opérateur valide.
 bool isOperator(std::string const &in)
 {
-	return (in == "+" || in == "-" || in == "*" || in == "/");
+    return in == "+" || in == "-" || in == "*" || in == "/";
 }
 
-// Effectue l'opération indiquée sur les deux nombres en haut de la pile.
 void doOperation(std::string const &in, std::stack<int> &stack)
 {
-	if (stack.size() < 2)
-		throw std::runtime_error(INVALID);
-	int a = stack.top();
-	stack.pop();
-	int b = stack.top();
-	stack.pop();
+    if (stack.size() < 2)
+        throw std::runtime_error(INVALID);
+    int a = stack.top();
+    stack.pop();
+    int b = stack.top();
+    stack.pop();
 
-	if (in == "+")
-		stack.push(b + a);
-	else if (in == "-")
-		stack.push(b - a);
-	else if (in == "*")
-		stack.push(b * a);
-	else if (in == "/") {
-        // Check for division by zero
+    if (in == "+")
+        stack.push(b + a);
+    else if (in == "-")
+        stack.push(b - a);
+    else if (in == "*")
+        stack.push(b * a);
+    else if (in == "/") {
         if (a == 0)
-            throw std::runtime_error("Division by zero error");
+            throw std::runtime_error(DIV_ZERO);
         stack.push(b / a);
     }
 }
 
-// Ajoute un nombre à la pile.
 void addNumber(std::string const &in, std::stack<int> &stack)
 {
-	int nb = std::atoi(in.c_str());
-
-	if (nb > 9)
-		throw std::runtime_error(ERROR);
-	stack.push(nb);
+    int nb = std::atoi(in.c_str());
+    if (nb > MAX_NUMBER)
+        throw std::runtime_error(TOO_LARGE);
+    stack.push(nb);
 }
 
-// Traite l'expression RPN et imprime le résultat.
 void rpn(std::string const &in)
 {
-	std::stack<int> stack;
-	std::stringstream ss(in);
-	std::string token;
+    if (in.empty())
+        throw std::runtime_error("Error: Empty input");
 
-	while (!ss.eof())
-	{
-		std::getline(ss, token, ' ');
-		if (token.empty())
-			continue;
-		if (isOperator(token))
-			doOperation(token, stack);
-		else if (isValidNumber(token))
-			addNumber(token, stack);
-		else
-			throw std::runtime_error(ERROR);
-	}
-	if (stack.size() == 1)
-		std::cout << stack.top() << std::endl;
-	else
-		throw std::runtime_error(ERROR);
+    std::stack<int> stack;
+    std::stringstream ss(in);
+    std::string token;
+
+    while (std::getline(ss, token, ' ')) {
+        if (token.empty())
+            continue;
+        if (isOperator(token))
+            doOperation(token, stack);
+        else if (isValidNumber(token))
+            addNumber(token, stack);
+        else
+            throw std::runtime_error(ERROR);
+    }
+
+    if (stack.size() != 1)
+        throw std::runtime_error(INCOMPLETE_EXPR);
+
+    std::cout << stack.top() << std::endl;
 }
+
--- cpp_module_09/ex01/RPN.hpp
+++ cpp_module_09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: nasamadi <nasamadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 15:41:21 by nasamadi          #+#    #+#             */
-/*   Updated: 2023/11/24 16:44:47 by nasamadi         ###   ########.fr       */
+/*   Updated: 2023/11/30 00:48:16 by nasamadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,7 @@
 #include <sstream>
 #include <stdexcept>
 
-// Définition des ensembles de caractères valides pour les nombres.
-#define VALID_SET	"0123456789"
-
+// Définition couleurs
 #define RESET	"\033[0m"
 #define RED		"\033[31m"
 #define GREEN	"\033[32m"
@@ -33,10 +31,18 @@
 #define BOLD_RED	"\e[1;31m"
 #define V_CYAN		"\e[0;38;5;44m"
 
+// Définition des ensembles de caractères valides pour les nombres.
+#define VALID_SET	"0123456789"
+
 // Messages d'erreur standards.
-#define ERROR	"Error"
-#define INVALID	"Invalid operations"
-#define USAGE	"Usage: ./rpn \"[expression]\""
+#define ERROR "Error: Error in expression"
+#define USAGE "Usage: ./rpn \"[expression]\""
+#define INVALID "Error: Invalid operation"
+#define DIV_ZERO "Error: Division by zero error"
+#define TOO_LARGE "Error: Number too large"
+#define INCOMPLETE_EXPR "Error: Incomplete expression"
+#define NON_NUMERIC "Error: Non-numeric character found"
+#define MAX_NUMBER 9
 
 bool isValidNumber(std::string const &in);
 bool isOperator(std::string const &in);
